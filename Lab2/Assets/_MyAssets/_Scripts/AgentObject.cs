98e805a633cde00262e355d4bc59eeb368a04de1
Iakov Iudakov <ejm3ndev@gmail.com>
2024-01-16 13:28:33 -0500

    done

---
+++ Lab2/Assets/_MyAssets/_Scripts/AgentObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentObject : MonoBehaviour
+{
+    //a parent class of all the agent objects
+    [SerializeField] Transform m_target;
+
+    public Vector3 TargetPosition
+    {
+        get { return m_target.position; }
+        set { m_target.position = value;}
+    }
+
+    void Start()
+    {
+        Debug.Log("Starting agent...");
+        TargetPosition = m_target.position;
+    }
+}
