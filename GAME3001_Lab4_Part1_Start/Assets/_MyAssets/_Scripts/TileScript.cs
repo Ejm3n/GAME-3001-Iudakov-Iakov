320170a9a7117ef963fd912118fd896ce3208cfe
Iakov Iudakov <ejm3ndev@gmail.com>
2024-02-13 12:45:22 -0500

    tilepanel added tilescript updated

--- GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/TileScript.cs
+++ GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/TileScript.cs
@@ -7,7 +7,8 @@ public class TileScript : MonoBehaviour
     [SerializeField] private GameObject[] neighbourTiles;
     [SerializeField] private Color original;
     public TileStatus status = TileStatus.UNVISITED;
-    
+    public TilePanel tilePanel;
+    public float cost = 999.9f;
     public void SetNeighbourTile(int index, GameObject tile)
     {
         neighbourTiles[index] = tile;
@@ -18,6 +19,49 @@ public class TileScript : MonoBehaviour
         if (!newColor)
             original = color;
         gameObject.GetComponent<SpriteRenderer>().color = color;
+    }
+    internal void SetStatus(TileStatus stat)
+    {
+        status = stat;
+
+        switch (status)
+        {
+            case TileStatus.UNVISITED:
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = original;
+                    tilePanel.statusText.text = "U";
+                    break;
+                }
+                case TileStatus.OPEN:
+                {
+                    tilePanel.statusText.text = "O";
+
+                    break;
+                }
+                case TileStatus.CLOSED:
+                {
+                    tilePanel.statusText.text = "C";
+                    break;
+                }
+                case TileStatus.IMPASSABLE:
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(.5f, 0f, 0f, .5f);
+                    tilePanel.statusText.text = "I";
+                    break;
+                }
+                case TileStatus.GOAL:
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, 0f, .5f);
+                    tilePanel.statusText.text = "G";
+                    break;
+                }
+                case TileStatus.START:
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, .5f, 0f, .5f);
+                    tilePanel.statusText.text = "S";
+                    break;
+                }
+        }
     }
 
     public void ToggleImpassable(bool impass = true)
