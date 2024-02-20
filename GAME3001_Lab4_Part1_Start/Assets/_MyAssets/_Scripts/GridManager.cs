8f595229980b2da79b8aa7de9535e4477709c3f4
Iakov Iudakov <ejm3ndev@gmail.com>
2024-02-20 13:38:30 -0500

    jopa

--- GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/GridManager.cs
+++ GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/GridManager.cs
@@ -138,7 +138,7 @@ public class GridManager : MonoBehaviour
         SetTileCosts(planetIndices);
     }
 
-    private void ConnectGrid()
+    public void ConnectGrid()
     {
         for (int row = 0; row < rows; row++)
         {
