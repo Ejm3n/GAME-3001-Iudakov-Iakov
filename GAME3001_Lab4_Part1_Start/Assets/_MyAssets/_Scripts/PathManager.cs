eb18115f8c09e7233377ba04568c90cadbe78a67
Iakov Iudakov <ejm3ndev@gmail.com>
2024-02-20 12:58:19 -0500

    pathmanager

---
+++ GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/PathManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PathNode
+{
+    public GameObject Tile { get; private set; }
+    public List<PathConnection> connections;
+
+    public PathNode(GameObject tile)
+    {
+        Tile = tile;
+        connections = new List<PathConnection> ();
+    }
+    public void AddConnections(PathConnection pathConnection)
+    {
+        connections.Add(pathConnection);
+    }
+
+}
+
+[System.Serializable]
+public class PathConnection
+{
+    public float Cost { get; set; }
+    public PathNode FromNode { get; set; }
+    public PathNode ToNode { get; set;}
+    public PathConnection(PathNode from, PathNode to, float cost = 1f)
+    {
+        FromNode = from;
+        ToNode = to;
+        Cost = cost;
+    }
+}
+public class NodeRecord
+{
+    public PathNode Node { get; set; }
+    public NodeRecord FromRecord { get; set; }
+    public PathConnection Connection { get; set; }
+    public float CostSoFar { get; set; }
+
+    public NodeRecord(PathNode node = null)
+    {
+        Node = node;
+        Connection = null;
+        FromRecord = null;
+        CostSoFar = 0f;
+
+    }
+ }
+
+
+public class PathManager : MonoBehaviour
+{
+    public List<NodeRecord> openList;
+    public List<NodeRecord> closeList;
+
+    public List<PathConnection> path;
+
+    public static PathManager Instance{get;private set;}
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            Initialize();
+        }
+           
+
+    }
+    private void Initialize()
+    {
+        openList = new List<NodeRecord>();
+        closeList = new List<NodeRecord> ();
+        path = new List<PathConnection>();
+    }
+
+    public void GetShortestPath()
+    {
+
+    }
+
+    public NodeRecord GetSmallestNode()
+    {
+        NodeRecord smallestNode = openList[0];
+
+        for(int i = 1; i< openList.Count;i++)
+        {
+            if (openList[i].CostSoFar<smallestNode.CostSoFar)
+            {
+                smallestNode = openList[i];
+            }
+            else if (openList[i].CostSoFar == smallestNode.CostSoFar)
+            {
+                smallestNode = (Random.value < 0.5 ? openList[i] : smallestNode);
+            }
+        }
+        return smallestNode;
+    }
+
+    public bool ContainsNode(List<NodeRecord> list, PathNode node)
+    {
+        foreach(NodeRecord record in list)
+        {
+            if(record.Node == node) return true;
+        }
+        return false;
+    }
+
+    public NodeRecord GetNodeRecord(List<NodeRecord> list,PathNode node)
+    {
+        foreach(NodeRecord record in list)
+        {
+            if(record.Node == node) return record;
+        }
+        return null;
+    }
+}
